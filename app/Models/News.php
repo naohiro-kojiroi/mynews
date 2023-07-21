92620bc8446cb43936e5ee7505bcb0508c914f28
[naohiro] <[nk2013m@gmail.com]>
2023-07-21 03:06:17 +0000

    PHP/Laravel(17)

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,11 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
-    //以下を追記
+    
     protected $guarded = array('id');
     
     public static $rules = array(
         'title' => 'required',
         'body' => 'required',
     );
+    
+    //News Modelに関連付けを行う
+    public function histories()
+    {
+        return $this->hasMany('App\Models\History');
+    }
 }
