e4460c367b428c6241ea5c21ec757ead265fea4e
[naohiro] <[nk2013m@gmail.com]>
2023-07-31 15:05:41 +0000

    PHP/Laravelデプロイ

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -7,7 +7,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 mx-auto">
-            <h2>ニュース新規作成</h2>
+            <h2>NEWS新規作成</h2>
             <form action="{{ route('admin.news.create') }}" method="post" enctype="multipart/form-data">
                 
                 @if (count($errors) > 0)
